a4a60ad040fc210c276cc4084823338335758f78
gihanchanaka <gihanchanaka@gmail.com>
2017-08-19 16:51:22 +0530

    Seasonal demand

--- Item_Demand.py
+++ Item_Demand.py
@@ -2,7 +2,7 @@
 import random
 import math
 import PoissonDristribution
-
+import random
 class Item_Demand():
     #The only functions you can use are
     #   constructor(averagePerDay)------ 0<=averagePerDay<=100
@@ -12,9 +12,14 @@ class Item_Demand():
     def __init__(self,averagePerDay):
         self.mean=averagePerDay
         self.sales = [0] * 365
-
+        randomPhase=random.random()*2*3.14
         for day in range(365):
-            self.sales[day]=round(PoissonDristribution.getNumberInPoissonDistribution(self.mean))
+            linear=self.mean*0.7
+            sinePart=self.mean*0.25
+            poissonPart=self.mean*0.3
+
+            saless=linear+sinePart*math.sin(randomPhase+(2*3.14*day/365))+PoissonDristribution.getNumberInPoissonDistribution(poissonPart)
+            self.sales[day]=round(saless)
 
     def salesOfDay(self,day):
         return self.sales[day]
